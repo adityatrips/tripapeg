6743fef9024fe62507f9c48f1e8149c9810c84b6
Aditya Tripathi <adityatripathi.at04@gmail.com>
2023-12-28 18:30:03 +0530

    changed buildscript

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+	apiKey: 'AIzaSyAXTygJensZK1ayGj0DNY5opyEqhzXKhv8',
+	authDomain: 'trip-a-peg.firebaseapp.com',
+	projectId: 'trip-a-peg',
+	storageBucket: 'trip-a-peg.appspot.com',
+	messagingSenderId: '54212365817',
+	appId: '1:54212365817:web:ff2dd01b1cdd3dba934128',
+};
+
+const app = initializeApp(firebaseConfig);
