4f5d57489ec18fd6947dd3d99dd0151d75f2020f
Aditya Tripathi <adityatripathi.at04@gmail.com>
2023-12-29 15:41:32 +0530

    changes

--- src/pages/PlaceSlug.jsx
+++ src/pages/PlaceSlug.jsx
@@ -49,70 +49,45 @@ const PlaceSlug = () => {
 		<div>Loading...</div>
 	) : (
 		<div>
-			<img
-				src={data.src}
-				alt={data.id}
-				className="w-[100%] flex justify-center items-center"
-			/>
-
-			<div className="flex flex-row justify-center items-center">
-				<div
-					data-duration={data.duration.split(' ')[0]}
-					className="duration rounded"
-				></div>
-				<h1 className="text-2xl">{data.name}</h1>
-			</div>
-
-			<div className="meta-dest">
-				<p className="text-justify">
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Est
-					modi, nostrum voluptate vitae esse dignissimos, consectetur
-					at laboriosam suscipit iusto aperiam eaque odio asperiores
-					minima perspiciatis atque ratione facere ullam nam! Fuga
-					minus in beatae libero. Voluptatibus impedit modi explicabo
-					earum ducimus, odit cupiditate ipsum quaerat. Corporis,
-					excepturi? Nam, quos?
-				</p>
-				<ul className="accordion">
-					<Accordion heading="Overview">
-						<DestCard
-							src={data.src}
-							title={data.name}
-							time={data.duration}
-							place={data.name}
-							price={`${data.price} / per person`}
-							jan={data.available.jan}
-							feb={data.available.feb}
-							mar={data.available.mar}
-							apr={data.available.apr}
-							may={data.available.may}
-							jun={data.available.jun}
-							jul={data.available.jul}
-							aug={data.available.aug}
-							sep={data.available.sep}
-							oct={data.available.oct}
-							nov={data.available.nov}
-							dec={data.available.dec}
-							inclusions={data.inclusions}
-							exclusions={data.exclusions || null}
-							hotels={data.hotels || null}
-						/>
-					</Accordion>
-					<Accordion heading="Itinerary">
-						{data &&
-							data.days.map((day) => (
-								<div key={day.day}>
-									<h1 className="text-xl font-extrabold">
-										Day {day.day} ({day.title})
-									</h1>
-									<p className="text-justify mb-5">
-										&nbsp;&nbsp;&nbsp;{day.desc}
-									</p>
-								</div>
-							))}
-					</Accordion>
-				</ul>
-			</div>
+			<ul className="accordion">
+				<Accordion heading="Overview">
+					<DestCard
+						src={data.src}
+						title={data.name}
+						time={data.duration}
+						place={data.name}
+						price={`${data.price} / per person`}
+						jan={data.available.jan}
+						feb={data.available.feb}
+						mar={data.available.mar}
+						apr={data.available.apr}
+						may={data.available.may}
+						jun={data.available.jun}
+						jul={data.available.jul}
+						aug={data.available.aug}
+						sep={data.available.sep}
+						oct={data.available.oct}
+						nov={data.available.nov}
+						dec={data.available.dec}
+						inclusions={data.inclusions}
+						exclusions={data.exclusions || null}
+						hotels={data.hotels || null}
+					/>
+				</Accordion>
+				<Accordion heading="Itinerary">
+					{data &&
+						data.days.map((day) => (
+							<div key={day.day}>
+								<h1 className="text-xl font-extrabold">
+									Day {day.day} ({day.title})
+								</h1>
+								<p className="text-justify mb-5">
+									&nbsp;&nbsp;&nbsp;{day.desc}
+								</p>
+							</div>
+						))}
+				</Accordion>
+			</ul>
 
 			<ul className="accordion">
 				<Accordion heading="Cancellation Policy">
