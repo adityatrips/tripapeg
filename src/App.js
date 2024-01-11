d6f1d7832e33b9be14b0e09e6b43a3a8f4656732
Aditya Tripathi <adityatripathi.at04@gmail.com>
2024-01-11 14:07:36 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -7,28 +7,7 @@ import PlacesSlug from './pages/PlaceSlug';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
-import { db } from './firebase';
-import { addDoc, collection, doc, setDoc } from 'firebase/firestore';
-import { notes } from './data';
-
 const App = () => {
-	useEffect(() => {
-		const docRef = collection(db, `notes`);
-		let addData = async () => {
-			try {
-				for (let i = 0; i < notes.length; i++) {
-					const ref = doc(docRef, `${i}`);
-
-					await setDoc(ref, {
-						desc: notes[i],
-					});
-				}
-			} catch (e) {
-				console.error(e);
-			}
-		};
-		addData();
-	}, []);
 	return (
 		<BrowserRouter>
 			<Navbar />
